f1d07cacf94802a4a66c18686a49b865b78a88b0
twalla26 <ssm4748@naver.com>
2026-08-11 14:21:31 +0900

    feat: Refresh Token 엔티티 구현

---
+++ src/main/kotlin/com/dandi/nyummy/auth/entity/RefreshToken.kt
@@ -0,0 +1,36 @@
+package com.dandi.nyummy.auth.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.Instant
+
+@Entity
+@EntityListeners(AuditingEntityListener::class)
+@Table(name = "refresh_token")
+class RefreshToken(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    val id: Long = 0L,
+
+    @Column(name = "refresh_token", nullable = false, length = 512)
+    var refreshToken: String,
+
+    @Column(name = "user_id", nullable = false)
+    var userId: Long = 0L,
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    var createdAt: Instant = Instant.now(),
+
+    @Column(name = "expires_at", nullable = false)
+    var expiresAt: Instant,
+)
