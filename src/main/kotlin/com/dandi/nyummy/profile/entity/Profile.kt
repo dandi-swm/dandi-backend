f2d4e7fbb75b73aab21449fdfd33fb867e5c1a89
twalla26 <ssm4748@naver.com>
2026-08-11 14:20:20 +0900

    refactor: profile 엔티티 수정
    - @Column 어노테이션에 name, nullable, unique, length 추가
    - updatedAt에 Auditing 어노테이션 추가

--- src/main/kotlin/com/dandi/nyummy/profile/entity/Profile.kt
+++ src/main/kotlin/com/dandi/nyummy/profile/entity/Profile.kt
@@ -2,45 +2,51 @@ package com.dandi.nyummy.profile.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 import java.time.LocalDate
 
 @Entity
+@EntityListeners(AuditingEntityListener::class)
 @Table(name = "profile")
 class Profile(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
+    @Column(name = "id", nullable = false)
+    val id: Long = 0L,
 
-    @Column
-    val userId: Long = 0,
-
-    @Column
+    @Column(name = "nickname", length = 100)
     val nickname: String? = null,
 
-    @Column
+    @Column(name = "birth")
     val birth: LocalDate? = null,
 
-    @Column
+    @Column(name = "gender")
     val gender: Byte? = null,
 
-    @Column
+    @Column(name = "height")
     val height: Int? = null,
 
-    @Column
+    @Column(name = "weight")
     val weight: Int? = null,
 
-    @Column
+    @Column(name = "coin", nullable = false)
+    val coin: Int = 0,
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    val userId: Long = 0L,
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
     val updatedAt: Instant? = null,
 
-    @Column
+    @Column(name = "last_login_at")
     val lastLoginAt: Instant? = null,
-
-    @Column
-    val coin: Int = 0,
 )
