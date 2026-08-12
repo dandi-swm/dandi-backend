ab976d7ca3c9df354817d85491ce7f5d4aa325ee
Vryez11 <vryez11@gmail.com>
2026-08-12 12:18:46 +0900

    docs: MailService public 메서드에 KDoc 추가
      - sendEmail/confirm에 MealService와 동일한 형식(@param/@throws)으로 주석 작성

--- src/main/kotlin/com/dandi/nyummy/auth/service/MailService.kt
+++ src/main/kotlin/com/dandi/nyummy/auth/service/MailService.kt
@@ -24,6 +24,12 @@ class MailService(
         private val EMAIL_VALID_TIME: Duration = Duration.ofMinutes(5)
     }
 
+    /**
+     * 인증 코드를 생성해 이메일로 발송한다. 기존 발송 이력이 있으면 코드를 새로 갱신해 재발송한다.
+     *
+     * @param email 인증 코드를 받을 이메일 주소
+     * @throws BusinessException [AuthErrorCode.MAIL_RESEND_TOO_EARLY] 마지막 발송 후 [EMAIL_VALID_TIME]이 지나지 않은 경우
+     */
     @Transactional
     fun sendEmail(email: String) {
         val existingMail = mailRepository.findByEmail(email)
@@ -56,6 +62,15 @@ class MailService(
 
     private fun createCode(): String = "%06d".format(random.nextInt(1_000_000))
 
+    /**
+     * 이메일로 발송된 인증 코드를 검증하고 해당 이메일을 인증 완료로 표시한다.
+     *
+     * @param email 인증할 이메일 주소
+     * @param code 사용자가 입력한 인증 코드
+     * @throws BusinessException [AuthErrorCode.MAIL_NOT_FOUND] 해당 이메일로 발송된 인증 코드가 없는 경우
+     * @throws BusinessException [AuthErrorCode.MAIL_CODE_EXPIRED] 발송 후 [EMAIL_VALID_TIME]이 지난 경우
+     * @throws BusinessException [AuthErrorCode.MAIL_CODE_MISMATCH] 인증 코드가 일치하지 않는 경우
+     */
     @Transactional
     fun confirm(email: String, code: String) {
         val existingMail = mailRepository.findByEmail(email)
